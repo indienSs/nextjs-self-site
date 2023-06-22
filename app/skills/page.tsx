b0a57b4c43990ef8f7903a161a1b6a8f4e975e0d
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-06-22 12:54:40 +0800

    pages

---
+++ app/skills/page.tsx
@@ -0,0 +1,10 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Навыки",
+  description: "Описание навыков Javascript",
+};
+
+export default function Skills() {
+  return <div>Мои навыки</div>;
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer>Ссылки на соц сети</footer>;
+}
